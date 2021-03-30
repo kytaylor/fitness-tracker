ef665ee321bcf93b0260402959563c3ee8f02158
kytaylor <ky.taylor5468@gmail.com>
2021-03-29 18:11:36 -0700

    adjusts case on workout.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const apiRoutes = require("./routes/routes_api");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/Workout',

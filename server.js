01fe0fb8c24e9c507ee30dc7cfbb0814d653d0bb
kytaylor <ky.taylor5468@gmail.com>
2021-03-29 17:22:33 -0700

    deploying

--- server.js
+++ server.js
@@ -11,7 +11,15 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 app.use(morgan("dev"));
 
